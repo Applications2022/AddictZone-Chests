1d8edf68179e2c599626642f4fbaf282f8ff364b
Ruben Fischer <ruben.fischer@terrarum.de>
2021-10-26 15:57:12 +0200

    first commit

---
+++ src/main/java/de/ruben/addictzonechests/AddictzoneChests.java
@@ -0,0 +1,17 @@
+package de.ruben.addictzonechests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AddictzoneChests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
